23056eeda2ee9c4791c005adb297b07174b59d52
iosdeveloperashish <iosdeveloperashish@gmail.com>
2021-04-01 12:32:22 +0530

    Project Created
    * Initial project setup

---
+++ LibertySolutionDemo/LibertySolutionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LibertySolutionDemo
+//
+//  Created by Ashish Viltoriya on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
